c4d2b3c0b8d3d5b898878d74b5c1989ebf307bae
Carlos Lorenzo <carloren.1996@gmail.com>
2025-06-02 11:25:08 +0000

    Ejercicios completados

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,20 @@
 // Your code here
+function deleteTaskListener() {
+    let trashes = document.querySelectorAll(".fa-trash")
+    trashes.forEach(trash => trash.addEventListener("click", () => {
+        let listElement = trash.parentNode.parentNode
+        listElement.parentNode.removeChild(listElement)
+    }))
+}
+
+let toDo = document.getElementById("addToDo")
+toDo.addEventListener("change", () => {
+    // alert(toDo.value)
+    let newlistElement = document.createElement("li")
+    newlistElement.innerHTML = `<span><i class="fa fa-trash"></i></span> ${toDo.value}`
+    document.querySelector("ul").appendChild(newlistElement)
+    toDo.value = ""
+    deleteTaskListener()
+})
+
+deleteTaskListener()
